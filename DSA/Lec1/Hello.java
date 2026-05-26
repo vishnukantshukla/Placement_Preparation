ade388c39a6dba34d57d32bf901433e3a0525457
vishnukantshukla <vishnukantshukla820@gmail.com>
2026-05-27 00:58:54 +0530

    first commit

---
+++ DSA/Lec1/Hello.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Hello {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello This side is Vishnukant Shukla");
+    }
+}
